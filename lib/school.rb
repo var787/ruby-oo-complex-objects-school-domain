07776e56bb9ccd61c1b974cec1a7418c740668e3
var787 <hegde.rakshan@gmail.com>
2020-10-28 01:44:01 -0500

    commit tree db808d5e5c8c3db79708a260a3e489c8071a1a92

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
-# code here!
+class School
+    
+    attr_reader :school_name, :roster
+    
+    def initialize(school_name)
+        @school_name = school_name
+        @roster={}
+    end
+    
+    def add_student(name,grade)
+        if @roster[grade]#if nil
+            @roster[grade]<<name
+        else
+            @roster[grade]=[] #if empty
+            @roster[grade]<<name
+        end
+        
+    end
+    
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        roster.each_value do |name|
+            name.sort! #original array changes instead of returning new one
+        end
+        roster
+        
+    end
+
+    
+end
